4e8d207fd2e9568f9d78b62d5e4e7550cc781d02
jp <30583333+jpcorona@users.noreply.github.com>
2020-06-22 02:36:56 -0400

    Add files via upload

--- LanzadorDcp/src/main/java/com/example/LanzadorDcp/ControllerDcp.java
+++ LanzadorDcp/src/main/java/com/example/LanzadorDcp/ControllerDcp.java
@@ -1,10 +1,9 @@
 package com.example.LanzadorDcp;
 
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
 
-import java.net.URI;
+
+
 import java.util.Arrays;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,25 +13,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
+
 
 
 @Controller
